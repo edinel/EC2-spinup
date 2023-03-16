caa2e08220c60d612ae203699113a4a57fcb0de2
Eddie Dinel <edinel@solace.org>
2023-03-16 12:57:31 -0700

    adding aws file

---
+++ aws-ec2-cmd.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/zsh
+#aws ec2 run-instances --image-id ami-0df24e148fdb9f1d8 --count 1 --instance-type t2.micro --subnet-id subnet-077b619fc7aa65b2a
+
+
+# aws ec2 run-instances --image-id ami-0df24e148fdb9f1d8 --count 1 --instance-type t2.micro
